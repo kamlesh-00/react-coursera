ea509b8a93330996699937cee28cf33708baea4f
Kamlesh Mistry <mistrykamlesh312@gmail.com>
2020-07-19 16:36:59 +0530

    Assignment 1(Done) Copied

---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import {Card,CardImg,CardText,CardTitle,CardBody} from 'reactstrap';
+
+class DishDetail extends Component{
+    renderDish(dish) {
+        if (dish != null)
+            return(
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return(
+                <div></div>
+            );
+    }
+
+    renderComments(comments){
+            if (comments != null) {
+                let options = { year: "numeric", month: "short", day: "numeric" };
+                return comments.map(comment => (
+                  <ul key={comment.id} className="list-unstyled">
+                    <li className="mb-2">{comment.comment}</li>
+                    <li>
+                      -- {comment.author}{" "}
+                      {new Date(comment.date).toLocaleDateString("en-US", options)}
+                    </li>
+                  </ul>
+                ));
+              } else return <div />;
+    }
+
+    render(){
+        return(
+            <div className="row">
+                <div  className="col-12 col-md-5 m-1">
+                    {this.renderDish(this.props.dish)}
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {this.renderComments(this.props.dish.comments)}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import { Media } from 'reactstrap';
-import {Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle} from 'reactstrap';
+import {Card,CardImg,CardImgOverlay,CardTitle} from 'reactstrap';
+import DishDetail from './DishDetailComponent';
 
 class Menu extends Component {
 
@@ -16,21 +17,12 @@ class Menu extends Component {
       this.setState({ selectedDish: dish});
   }
 
-  renderDish(dish) {
-      if (dish != null)
-          return(
-              <Card>
-                  <CardImg top src={dish.image} alt={dish.name} />
-                  <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                  </CardBody>
-              </Card>
-          );
-      else
-          return(
-              <div></div>
-          );
+  renderDish(dish){
+      if(dish!=null){
+          return <DishDetail dish={dish} />;
+      }else{
+          return <div />;
+      }
   }
 
   render() {
@@ -53,11 +45,7 @@ class Menu extends Component {
               <div className="row">
                   {menu}
               </div>
-              <div className="row">
-                <div  className="col-12 col-md-5 m-1">
-                  {this.renderDish(this.state.selectedDish)}
-                </div>
-              </div>
+              {this.renderDish(this.state.selectedDish)}
           </div>
       );
   }
